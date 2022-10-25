58326232f08a0306bd4471708d18bc3eef8efba7
Kyle Ledin <Kyle.Ledin85@gmail.com>
2022-10-25 13:33:39 -0500

    Have all of iteration 3 complete besides details method and all of iteration 4 besides average cost of player method not displaying as a string

--- lib/team.rb
+++ lib/team.rb
@@ -1,15 +1,44 @@
 class Team
-  attr_reader :team_name, :city, :roster, :player_count
+  attr_reader :team_name, :city,
+              :roster,    :player_count,
+              :long_term_players,
+              :short_term_players
 
   def initialize(team_name, city)
     @team_name = team_name
     @city = city
     @roster = []
     @player_count = 0
+    @long_term_players = []
+    @short_term_players = []
   end
 
   def add_player(player)
     @roster << player
     @player_count += 1
+    if player.contract_length > 24
+      @long_term_players << player
+    else
+      @short_term_players << player
+    end
+  end
+
+  def total_value
+    @roster.map do |player|
+      player.total_cost
+    end.sum
+  end
+
+  def average_cost_of_player
+    @roster.map do |player|
+      player.total_cost
+    end.sum / @roster.length
+  end
+
+  def players_by_last_name
+    last_names =  @roster.map do |player|
+      player.last_name
+    end
+    last_names.sort { |a, b| a <=> b }
   end
 end
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -40,4 +40,101 @@ RSpec.describe Team do
 
     expect(team.player_count).to eq(2)
   end
+
+  it 'has long term players' do
+    team = Team.new("Dodgers", "Los Angeles")
+
+    expect(team.long_term_players).to eq([])
+  end
+
+  it 'has short term players' do
+    team = Team.new("Dodgers", "Los Angeles")
+
+    expect(team.short_term_players).to eq([])
+  end
+
+  it 'can add long term players' do
+    team = Team.new("Dodgers", "Los Angeles")
+    player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    player_2 = Player.new("Kenny DeNunez", 500000, 24)
+    player_3 = Player.new("Alan McClennan", 750000, 48)
+    player_4 = Player.new("Hamilton Porter", 100000, 12)
+    team.add_player(player_1)
+    team.add_player(player_2)
+    team.add_player(player_3)
+    team.add_player(player_4)
+
+    expect(team.long_term_players).to eq([player_1, player_3])
+  end
+
+  it 'can add short term players' do
+    team = Team.new("Dodgers", "Los Angeles")
+    player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    player_2 = Player.new("Kenny DeNunez", 500000, 24)
+    player_3 = Player.new("Alan McClennan", 750000, 48)
+    player_4 = Player.new("Hamilton Porter", 100000, 12)
+    team.add_player(player_1)
+    team.add_player(player_2)
+    team.add_player(player_3)
+    team.add_player(player_4)
+
+    expect(team.short_term_players).to eq([player_2, player_4])
+  end
+
+  it 'has a total value' do
+    team = Team.new("Dodgers", "Los Angeles")
+    player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    player_2 = Player.new("Kenny DeNunez", 500000, 24)
+    player_3 = Player.new("Alan McClennan", 750000, 48)
+    player_4 = Player.new("Hamilton Porter", 100000, 12)
+    team.add_player(player_1)
+    team.add_player(player_2)
+    team.add_player(player_3)
+    team.add_player(player_4)
+
+    expect(team.total_value).to eq(85200000)
+  end
+
+  it "has details" do
+    team = Team.new("Dodgers", "Los Angeles")
+    player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    player_2 = Player.new("Kenny DeNunez", 500000, 24)
+    player_3 = Player.new("Alan McClennan", 750000, 48)
+    player_4 = Player.new("Hamilton Porter", 100000, 12)
+    team.add_player(player_1)
+    team.add_player(player_2)
+    team.add_player(player_3)
+    team.add_player(player_4)
+
+    expect(team.details).to eq({"total_value" => 85200000, "player_count" => 4})
+
+  end
+
+  it "has an average cost of player" do
+    team = Team.new("Dodgers", "Los Angeles")
+    player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    player_2 = Player.new("Kenny DeNunez", 500000, 24)
+    player_3 = Player.new("Alan McClennan", 750000, 48)
+    player_4 = Player.new("Hamilton Porter", 100000, 12)
+    team.add_player(player_1)
+    team.add_player(player_2)
+    team.add_player(player_3)
+    team.add_player(player_4)
+
+    expect(team.average_cost_of_player).to eq("$21,300,000")
+  end
+
+  it "can look at player by last name" do
+    team = Team.new("Dodgers", "Los Angeles")
+    player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    player_2 = Player.new("Kenny DeNunez", 500000, 24)
+    player_3 = Player.new("Alan McClennan", 750000, 48)
+    player_4 = Player.new("Hamilton Porter", 100000, 12)
+    team.add_player(player_1)
+    team.add_player(player_2)
+    team.add_player(player_3)
+    team.add_player(player_4)
+
+    expect(team.players_by_last_name).to eq(["DeNunez", "McClennan", "Palledorous", "Porter"])
+  end
 end
